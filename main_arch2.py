bbd548a6d7ab8daaa1a47caed4538bdba0ee3262
ivan_yu <gun123kk@gmail.com>
2021-10-31 17:40:35 +0800

    add arch2
    ./run_arch2.sh

---
+++ main_arch2.py
@@ -0,0 +1,33 @@
+import mot_class_arch2 as mtc
+import os              
+import argparse
+import yolo_object_detection as yolo_obj
+import cv2
+import imutils
+
+
+def read_user_input_info():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video", required=True,
+        help="path to input video file")
+    args = vars(ap.parse_args())
+    return args
+
+
+if __name__ == '__main__':
+    # construct the argument parser and parse the arguments
+    args = read_user_input_info()
+    vs = cv2.VideoCapture(args["video"])
+    (grabbed, frame) = vs.read()
+    resize_width = 1280
+    frame = imutils.resize(frame, width=resize_width)
+    (h, w) = frame.shape[:2]
+
+    yolo = yolo_obj.yolo_object_detection()
+    bboxes = []
+    bboxes = yolo.run_detection(frame, 'person')
+
+    print(bboxes)
+    # load our serialized model from disk                                   
+    mc = mtc.mot_class_arch2(bboxes, frame, resize_width)
+    mc.tracking(args)
