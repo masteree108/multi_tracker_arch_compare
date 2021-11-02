28cb0f9ef78552349985f493d8ca5b7b94d0df29
DNS-0522 <xkoukix111@gmail.com>
2021-11-03 00:43:48 +0800

    Add full-screen Detection after Crop Detection

--- yolo_object_detection.py
+++ yolo_object_detection.py
@@ -11,14 +11,14 @@ import os
 
 
 class yolo_object_detection():
-# private
+    # private
     __labelsPath = "./yolo-coco_v4/coco.names"
     __weightsPath = "yolo-coco_v4/yolov4.weights"
     __configPath = "yolo-coco_v4/yolov4.cfg"
     __confidence_setting = 0.5
     __threshold = 0.1
 
-# public
+    # public
     def __init__(self):
         np.random.seed(42)
         self.__LABELS = open(self.__labelsPath).read().strip().split("\n")
@@ -27,7 +27,6 @@ class yolo_object_detection():
         # derive the paths to the YOLO weights and model configuration
         # load our YOLO object detector trained on COCO dataset (80 classes)
         self.__net = cv2.dnn.readNetFromDarknet(self.__configPath, self.__weightsPath)
-
 
     def run_detection(self, frame, specify_obj):
         # determine only the *output* layer names that we need from YOLO
@@ -51,47 +50,82 @@ class yolo_object_detection():
         # Add a switch to decide whether to cutout pictures to improve YOLO
         # Each time crop a 1920*1080 from frame to YOLO
         # Overlap 50%
-        if crop_switch==True:
-            if crop_switch:
-                for crop_x in range(0, 2840, 960):
-                    for crop_y in range(0, 1620, 540):
-                        crop_img = frame[crop_y:crop_y + crop_high, crop_x:crop_x + crop_width]
-                        blob = cv2.dnn.blobFromImage(crop_img, 1 / 255.0, (416, 416),
-                                                     swapRB=True, crop=False)
-                        self.__net.setInput(blob)
-                        layerOutputs = self.__net.forward(ln)
-                        # loop over each of the layer outputs
-                        for output in layerOutputs:
-                            # loop over each of the detections
-                            for detection in output:
-                                # extract the class ID and confidence (i.e., probability)
-                                # of the current object detection
-                                scores = detection[5:]
-                                classID = np.argmax(scores)
-                                confidence = scores[classID]
+        if crop_switch == True:
+            for crop_x in range(0, 2840, 960):
+                for crop_y in range(0, 1620, 540):
+                    crop_img = frame[crop_y:crop_y + crop_high, crop_x:crop_x + crop_width]
+                    blob = cv2.dnn.blobFromImage(crop_img, 1 / 255.0, (416, 416),
+                                                 swapRB=True, crop=False)
+                    self.__net.setInput(blob)
+                    layerOutputs = self.__net.forward(ln)
+                    # loop over each of the layer outputs
+                    for output in layerOutputs:
+                        # loop over each of the detections
+                        for detection in output:
+                            # extract the class ID and confidence (i.e., probability)
+                            # of the current object detection
+                            scores = detection[5:]
+                            classID = np.argmax(scores)
+                            confidence = scores[classID]
 
-                                # filter out weak predictions by ensuring the detected
-                                # probability is greater than the minimum probability
-                                if confidence > self.__confidence_setting:
-                                    # scale the bounding box coordinates back relative to
-                                    # the size of the image, keeping in mind that YOLO
-                                    # actually returns the center (x, y)-coordinates of
-                                    # the bounding box followed by the boxes' width and
-                                    # height
-                                    box = detection[0:4] * np.array([crop_width, crop_high, crop_width, crop_high])
-                                    (centerX, centerY, width, height) = box.astype("int")
-                                    if width < crop_width * 2 / 3:
-                                        # use the center (x, y)-coordinates to derive the top
-                                        # and and left corner of the bounding box
-                                        x = int(centerX - (width / 2))
-                                        y = int(centerY - (height / 2))
+                            # filter out weak predictions by ensuring the detected
+                            # probability is greater than the minimum probability
+                            if confidence > self.__confidence_setting:
+                                # scale the bounding box coordinates back relative to
+                                # the size of the image, keeping in mind that YOLO
+                                # actually returns the center (x, y)-coordinates of
+                                # the bounding box followed by the boxes' width and
+                                # height
+                                box = detection[0:4] * np.array([crop_width, crop_high, crop_width, crop_high])
+                                (centerX, centerY, width, height) = box.astype("int")
+                                if width < crop_width * 2 / 3:
+                                    # use the center (x, y)-coordinates to derive the top
+                                    # and and left corner of the bounding box
+                                    x = int(centerX - (width / 2))
+                                    y = int(centerY - (height / 2))
 
-                                        # update our list of bounding box coordinates,
-                                        # confidences, and class IDs
-                                        boxes.append([crop_x + x, crop_y + y, int(width), int(height)])
-                                        confidences.append(float(confidence))
-                                        classIDs.append(classID)
-        else :
+                                    # update our list of bounding box coordinates,
+                                    # confidences, and class IDs
+                                    boxes.append([crop_x + x, crop_y + y, int(width), int(height)])
+                                    confidences.append(float(confidence))
+                                    classIDs.append(classID)
+            blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+                                         swapRB=True, crop=False)
+            self.__net.setInput(blob)
+            layerOutputs = self.__net.forward(ln)
+            # initialize our lists of detected bounding boxes, confidences,
+            # and class IDs, respectively
+            # loop over each of the layer outputs
+            for output in layerOutputs:
+                # loop over each of the detections
+                for detection in output:
+                    # extract the class ID and confidence (i.e., probability)
+                    # of the current object detection
+                    scores = detection[5:]
+                    classID = np.argmax(scores)
+                    confidence = scores[classID]
+
+                    # filter out weak predictions by ensuring the detected
+                    # probability is greater than the minimum probability
+                    if confidence > self.__confidence_setting:
+                        # scale the bounding box coordinates back relative to
+                        # the size of the image, keeping in mind that YOLO
+                        # actually returns the center (x, y)-coordinates of
+                        # the bounding box followed by the boxes' width and
+                        # height
+                        box = detection[0:4] * np.array([W, H, W, H])
+                        (centerX, centerY, width, height) = box.astype("int")
+
+                        # use the center (x, y)-coordinates to derive the top
+                        # and and left corner of the bounding box
+                        x = int(centerX - (width / 2))
+                        y = int(centerY - (height / 2))
+                        # update our list of bounding box coordinates,
+                        # confidences, and class IDs
+                        boxes.append([x, y, int(width), int(height)])
+                        confidences.append(float(confidence))
+                        classIDs.append(classID)
+        else:
             blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
                                          swapRB=True, crop=False)
             self.__net.setInput(blob)
@@ -153,6 +187,5 @@ class yolo_object_detection():
                     text = "{}: {:.4f}".format(self.__LABELS[classIDs[i]], confidences[i])
                     cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-        cv2.imwrite("frame.jpg",frame)
+        cv2.imwrite("frame.jpg", frame)
         return return_bboxes
-
